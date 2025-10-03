da4e1d964307e72c9ad1e8ffed00a503b0d449d1
Daria Polubenko <daria.polubenko@gmail.com>
2025-10-03 17:51:27 +0100

    rename class TwoSum -> Solution.java

--- src/main/java/leetcode/easy/two_sum/Solution.java
+++ src/main/java/leetcode/easy/two_sum/Solution.java
@@ -1,6 +1,6 @@
 package leetcode.easy.two_sum;
 
-public class TwoSum {
+public class Solution {
     public static int[] twoSum (int[] nums, int target) {
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
